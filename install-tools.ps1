70d615a49ec1f4c7da88cdd6f06b6bd9a8090c80
zadykian <zadykian@outlook.com>
2022-05-17 17:10:01 +0300

    install-tools.ps1

---
+++ install-tools.ps1
@@ -0,0 +1,38 @@
+$installRootLocation = "C:\Tools"
+
+$packagesToInstall = @(
+	"Google.Chrome",
+	"Telegram.TelegramDesktop",
+	"FinalWire.AIDA64.Extreme",
+
+	"Microsoft.dotnet",
+	"DevToys",
+	"Microsoft.PowerShell",
+	"Git.Git",
+	"Docker.DockerDesktop",
+
+	"JetBrains.dotUltimate",
+	"JetBrains.DataGrip",
+
+	"Microsoft.VisualStudioCode",
+	"Microsoft.VisualStudio.2022.Community",
+
+	"Microsoft Remote Desktop",
+	"mRemoteNG.mRemoteNG",
+	"AnyConnect",
+	"OpenVPNTechnologies.OpenVPNConnect"
+)
+
+$installedPackages = winget list
+
+function PackageIsNotInstalled([string]$packageId) {
+	$occurrences = $installedPackages | Where-Object { $_ -match $packageId }
+	$occurrences.Count -eq 0
+}
+
+$notInstalledPackages = $packagesToInstall | Where-Object { PackageIsNotInstalled $_ }
+
+foreach ($packageId in $notInstalledPackages) {
+	$installLocation = Join-Path $installRootLocation $packageId
+	winget install $packageId --location $installLocation
+}
